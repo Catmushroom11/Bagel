9fa4170aa40ea8647c359193621fe6dbf9a3f4b1
Catmushroom11 <109758632+Catmushroom11@users.noreply.github.com>
2022-08-16 18:25:00 -0500

    Create Inside Code.py

---
+++ Inside Code.py
@@ -0,0 +1,8 @@
+"""Bagels, by Al Sweigart al@inventwithpython.com
+A deductive logic game where you must guess a number based on clues.
+View this code at https://nostarch.com/big-book-of-small-python-project
+A version of this game is featured in the book "Invent Your Own 
+Computer Games With Python" https://nostarch.com/inventeithpython
+Tags: short, game, puzzle"""
+
+import random
